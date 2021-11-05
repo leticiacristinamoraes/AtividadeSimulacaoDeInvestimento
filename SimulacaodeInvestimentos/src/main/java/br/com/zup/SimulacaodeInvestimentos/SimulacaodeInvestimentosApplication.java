8363eef9b3a0365e372dcfd7c385efe63947cfb9
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-05 10:41:44 -0300

    Criar projeto

---
+++ SimulacaodeInvestimentos/src/main/java/br/com/zup/SimulacaodeInvestimentos/SimulacaodeInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.SimulacaodeInvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacaodeInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacaodeInvestimentosApplication.class, args);
+	}
+
+}
---
+++ SimulacaodeInvestimentos/src/test/java/br/com/zup/SimulacaodeInvestimentos/SimulacaodeInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.SimulacaodeInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacaodeInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
